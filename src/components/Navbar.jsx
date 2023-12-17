65c497d9eb75dc756d73fe2d8b608d2b479009bf
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 16:15:30 -0500

    Add Container style-component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black
+`
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>Navbar</Container>
   )
 }
 
