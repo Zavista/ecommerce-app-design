4edd849e67a845f8e2c40468b8e5648201a8b4c6
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 16:40:41 -0500

    Added styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,52 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Container = styled.div`
-    height: 60px;
-    background-color: #FDF7E4
-`
+  height: 60px;
+  background-color: #fdf7e4;
+`;
 const Wrapper = styled.div`
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
 const Left = styled.div`
-    flex: 1;
-`
+  flex: 1;
+  display: flex;
+  align-items: center
+`;
 const Center = styled.div`
-    flex: 1;
-`
+  flex: 1;
+`;
 const Right = styled.div`
-    flex: 1
-`
+  flex: 1;
+`;
 
 const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items:center
 `
-
-
 
 const Navbar = () => {
   return (
     <Container>
-        <Wrapper>
-            <Left>
-                <Language>EN</Language>
-            </Left>
-            <Center>
-            center</Center>
-            <Right>right</Right>
-        </Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>search <SearchIcon></SearchIcon></SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
