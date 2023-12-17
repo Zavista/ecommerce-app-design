91d358b6fe4cfa3de9f8d9bebf8ada03ae2bce6e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 16:18:57 -0500

    Create Wrapper styled component in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color: black
+    background-color: #FDF7E4
+`
+const Wrapper = styled.div`
+    padding: 10px 20px
 `
 
 const Navbar = () => {
   return (
-    <Container>Navbar</Container>
+    <Container>
+        <Wrapper>
+            Navvar
+        </Wrapper>
+    </Container>
   )
 }
 
