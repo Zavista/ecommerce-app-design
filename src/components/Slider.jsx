a7e7452ea90b306653abc8ba891c8b7d79696426
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 20:32:33 -0500

    Updated styling for Arrow

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,25 +3,36 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Container = styled.div`
-    background-color: lightblue; //just to allow me to see the total container while coding
     width: 100%;
     height: 100vh;
     display: flex;
+    position: relative;
 `
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: #3E2723;
+    background-color: tan;
     border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top:0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
 `
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left">
             <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
         </Arrow>
-        <Arrow>
+        <Arrow direction="right">
             <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
         </Arrow>
     </Container>
