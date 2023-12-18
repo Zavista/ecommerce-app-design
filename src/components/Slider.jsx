11736f9f95e424a231613b7103f1a1734ba811fe
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 22:56:00 -0500

    Added more Slides to Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,95 +1,134 @@
-import styled from "styled-components"
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import styled from "styled-components";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    position: relative;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    background-color: tan;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top:0;
-    bottom: 0;
-    left: ${props => props.direction === "left" && "10px"};
-    right: ${props => props.direction === "right" && "10px"};
-    margin: auto;
-    cursor: pointer;
-    opacity: 0.5;
-`
+  width: 50px;
+  height: 50px;
+  background-color: transparent;
+  font-weight: 1000;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "15px"};
+  right: ${(props) => props.direction === "right" && "15px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
 
 const Wrapper = styled.div`
-    height: 100%;
-`
+  height: 100%;
+  display: flex;
+`;
 
 const Slide = styled.div`
-    display: flex;
-    align-items:center;
-    width: 100vw;
-    height: 100vh;
-`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 const ImageContainer = styled.div`
-    flex: 1;
-    height: 100%
-`
+  flex: 1;
+  height: 100%;
+`;
 
 const Image = styled.img`
-    height: 80%
-`
+  height: 80%;
+`;
 const InfoContainer = styled.div`
-    flex: 1;
-    padding: 50px;
-    
-`
+  flex: 1;
+  padding: 50px;
+`;
 const Title = styled.h1`
-    font-size: 70px;
-`
+  font-size: 70px;
+`;
 const Description = styled.p`
-    margin: 50px 0px;
-    font-size: 20px;
-    font-weight: 500;
-    letter-spacing: 3px;
-`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 2px;
+`;
 const Button = styled.button`
-    padding: 8px 10px;
-    font-size: 20px;
-    background-color: transparent;
-    cursor: pointer;
-    
-`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  letter-spacing: 2px;
+`;
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow direction="left">
-            <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
-        </Arrow>
-        <Wrapper>
-            <Slide>            
-                <ImageContainer>
-                    <Image src="https://iili.io/H8Y78Qt.webp"></Image>
-                </ImageContainer>
-                <InfoContainer>
-                    <Title>Summer Sale</Title>
-                    <Description>Don&apos;t Compromise on Coffee! Get A Flat 30% On Everything</Description>
-                    <Button>Shop Now</Button>
-                </InfoContainer>
-            </Slide>
-
-        </Wrapper>
-        <Arrow direction="right">
-            <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
-        </Arrow>
+      <Arrow direction="left">
+        <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImageContainer>
+            <Image src="https://iili.io/H8Y78Qt.webp"></Image>
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Summer Sale</Title>
+            <Description>
+                Don&apos;t Compromise on Style! Get A Flat 30% Off For New Arrivals
+            </Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImageContainer>
+            <Image src="https://iili.io/H8Y78Qt.webp"></Image>
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Summer Sale</Title>
+            <Description>
+              Don&apos;t Compromise on Coffee! Get A Flat 30% On Everything
+            </Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImageContainer>
+            <Image src="https://iili.io/H8Y78Qt.webp"></Image>
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Summer Sale</Title>
+            <Description>
+              Don&apos;t Compromise on Style! Get A Flat 30% Off For New Arrivals
+            </Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImageContainer>
+            <Image src="https://iili.io/H8Y78Qt.webp"></Image>
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Summer Sale</Title>
+            <Description>
+              Don&apos;t Compromise on Coffee! Get A Flat 30% On Everything
+            </Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
+      </Arrow>
     </Container>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
