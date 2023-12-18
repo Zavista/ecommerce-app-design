00fb7f14e473d06f52e1790e959ee2bd8e0d6e38
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 17:23:05 -0500

    Create Newsletter Component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,20 @@
+import { Send } from "@mui/icons-material"
+import styled from "styled-components"
+
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title></Title>
+        <Description></Description>
+        <InputContainer>
+            <Input></Input>
+            <Button>
+                <Send></Send>
+            </Button>
+        </InputContainer>
+    </Container>
+  )
+}
+
+export default Newsletter
