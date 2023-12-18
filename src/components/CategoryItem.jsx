ae6c9a17de264b40a055873f60293cf616227a9f
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 01:45:54 -0500

    Moved Info and Button ontop of Image

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components"
 
 const Container = styled.div`
   flex: 1;
-  margin: 3px;
+  margin: 0px;
   height: 70vh;
+  position: relative;
 `
 const Image = styled.img`
   width: 100%;
@@ -11,13 +12,27 @@ const Image = styled.img`
   object-fit: cover;
 `
 const Info = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
 `
 
 const Title = styled.h1`
-    
+  color: black;
+  margin-bottom: 5px;
 `
 const Button = styled.button`
-    
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
 `
 
 const CategoryItem = ({item}) => {
