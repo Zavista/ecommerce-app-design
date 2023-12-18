241700a6b09bf625d08ebecd3926867ee1d46bda
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 20:23:35 -0500

    Add arrow and container to Slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,30 @@
+import styled from "styled-components"
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+const Container = styled.div`
+    background-color: lightblue; //just to allow me to see the total container while coding
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #3E2723;
+    border-radius: 50%;
+`
+
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <Container>
+        <Arrow>
+            <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
+        </Arrow>
+        <Arrow>
+            <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
+        </Arrow>
+    </Container>
   )
 }
 
