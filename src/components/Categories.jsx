7d97e961fc10d40654597a9c1c6a1289f8eab29d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 15:54:48 -0500

    Added Title component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,20 +2,37 @@ import styled from "styled-components";
 import { categories } from "../data";
 import CategoryItem from "./CategoryItem";
 
+
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
+const CategoryContainer = styled.div`
     display: flex;
     padding: 0px;
     justify-content: space-between;
     margin: 0px;
 `
 
+const Title = styled.h1`
+  font-weight: 700;
+  font-size: 40px;
+  margin-top: 20px;
+  margin-bottom: 25px;
+`
+
 const Categories = () => {
   return (
     <Container>
-        {categories.map(item=>(
-          <CategoryItem item={item} key={item.id}>
-          </CategoryItem>
-      ))}
+      <Title>Discover Our Global Coffee Origins</Title>
+        <CategoryContainer>
+          {categories.map(item=>(
+            <CategoryItem item={item} key={item.id}>
+            </CategoryItem>
+        ))}
+      </CategoryContainer>
     </Container>
   )
 
