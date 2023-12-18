ce4e7a6dad6b11bed1827ff926b5b5ce35f9f7ac
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 17:50:21 -0500

    Add logo and icons to Left

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, Twitter } from "@mui/icons-material"
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -14,11 +15,41 @@ const Center = styled.div`
 const Right = styled.div`
     flex: 1;
 `
+const Logo = styled.h1`
+    
+`
+const Description = styled.p`
+    
+`
+
+const SocialContainer = styled.div`
+    
+`
+const SocialIcon = styled.div`
+    
+`
 
 const Footer = () => {
   return (
     <Container>
-        <Left></Left>
+        <Left>
+            <Logo>Brewtiful</Logo>
+            <Description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae incidunt aliquid possimus reprehenderit deleniti, temporibus quam provident rerum quasi quibusdam necessitatibus dignissimos nesciunt, voluptatibus earum! Ut tenetur magnam perferendis?</Description>
+            <SocialContainer>
+                <SocialIcon>
+                    <Facebook></Facebook>
+                </SocialIcon>
+                <SocialIcon>
+                    <Instagram></Instagram>
+                </SocialIcon>
+                <SocialIcon> 
+                    <Twitter></Twitter>
+                </SocialIcon>
+                
+                
+               
+            </SocialContainer>
+        </Left>
         <Center></Center>
         <Right></Right>
     </Container>
