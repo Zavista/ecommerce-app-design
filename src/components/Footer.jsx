10a9511d38be2845106d49af31d11c02641ea315
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 18:23:04 -0500

    Added styling to Left portion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,59 +1,76 @@
-import { Facebook, Instagram, Twitter } from "@mui/icons-material"
-import styled from "styled-components"
+import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material";
+import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 const Left = styled.div`
-    flex: 1;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1`
+
+`;
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+    display: flex;
+`;
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: ${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 15px;
+`;
+
+
 const Center = styled.div`
-    flex: 1;
-`
+  flex: 1;
+`;
 
 const Right = styled.div`
-    flex: 1;
-`
-const Logo = styled.h1`
-    
-`
-const Description = styled.p`
-    
-`
+  flex: 1;
+`;
 
-const SocialContainer = styled.div`
-    
-`
-const SocialIcon = styled.div`
-    
-`
 
 const Footer = () => {
   return (
     <Container>
-        <Left>
-            <Logo>Brewtiful</Logo>
-            <Description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum beatae incidunt aliquid possimus reprehenderit deleniti, temporibus quam provident rerum quasi quibusdam necessitatibus dignissimos nesciunt, voluptatibus earum! Ut tenetur magnam perferendis?</Description>
-            <SocialContainer>
-                <SocialIcon>
-                    <Facebook></Facebook>
-                </SocialIcon>
-                <SocialIcon>
-                    <Instagram></Instagram>
-                </SocialIcon>
-                <SocialIcon> 
-                    <Twitter></Twitter>
-                </SocialIcon>
-                
-                
-               
-            </SocialContainer>
-        </Left>
-        <Center></Center>
-        <Right></Right>
+      <Left>
+        <Logo>Brewtiful</Logo>
+        <Description>
+          Indulge in exceptional coffee blends curated for your perfect brew.
+          Elevate your coffee experience with our handpicked selection of
+          premium beans sourced from around the globe.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="#3b5999">
+            <Facebook></Facebook>
+          </SocialIcon>
+          <SocialIcon color="#e4405f">
+            <Instagram></Instagram>
+          </SocialIcon>
+          <SocialIcon color="#55acee">
+            <Twitter></Twitter>
+          </SocialIcon>
+          <SocialIcon color="#e60023">
+            <Pinterest></Pinterest>
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center></Center>
+      <Right></Right>
     </Container>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
