61325b19d161886900972dc03fa1fda86a59f310
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 16:11:34 -0500

    Added styled-components to Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,37 @@
 import styled from "styled-components"
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
-const Container = sytled.div`
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+`
+const Circle = styled.div`
     
 `
-const Product = () => {
+const Image = styled.img`
+    
+`
+
+const Info = styled.div`
+    
+`
+const Icon = styled.div`
+    
+`
+const Product = (item) => {
   return (
     <Container>
-        
+        <Circle></Circle>
+        <Image src={item.image_url}></Image>
+        <Info>
+            <Icon>
+                <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon>
+                <SearchOutlinedIcon></SearchOutlinedIcon>
+                <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
+            </Icon>
+        </Info>
     </Container>
   )
 }
