9a7d0f26e77c897630ab6de134a7c275907cc425
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 01:36:49 -0500

    Added styled components for CategoryItem

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,12 +1,33 @@
 import styled from "styled-components"
 
 const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Info = styled.div`
+`
+
+const Title = styled.h1`
     
 `
-const CategoryItem = (item) => {
+const Button = styled.button`
+    
+`
+
+const CategoryItem = ({item}) => {
   return (
     <Container>
-
+        <Image src={item.img}></Image>
+        <Info>
+            <Title>{item.title}</Title>
+            <Button>Shop Now</Button>
+        </Info>
     </Container>
   )
 }
