bd2be25392c1e49351e36f3a4c6c0a11c51b50b1
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 17:34:41 -0500

    Added styling to components

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,31 +2,48 @@ import { Send } from "@mui/icons-material"
 import styled from "styled-components"
 
 const Container = styled.div`
-    height: 60vh;
+    height: 40vh;
     background-color: #fcf5f5;
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
 `
 
 const Title = styled.h1`
-    
+    font-size: 70px;
+    margin-bottom: 20px;
 `
 
 const Description = styled.div`
-    
+    font-size: 24px;
+    font-weight: 400;
+    margin-bottom: 20px;
 `
 
 const InputContainer = styled.div`
-    
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
 `
 
 const Input = styled.input`
-    
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+    outline: none;
+    border: 1px solid teal;
+    box-sizing: border-box;
 `
 
 const Button = styled.button`
-    
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
 `
 
 
@@ -37,7 +54,7 @@ const Newsletter = () => {
         <Title>Newsletter</Title>
         <Description>Get timely updates on your favorite products. </Description>
         <InputContainer>
-            <Input></Input>
+            <Input placeholder="Enter your email"></Input>
             <Button>
                 <Send></Send>
             </Button>
