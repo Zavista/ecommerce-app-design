a1f4f45017422c4db5b56cfcf2ad360796c96e41
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 19:57:19 -0500

    Updated SearchIcon styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const SearchContainer = styled.div`
     display: flex;
     align-items: center;
     margin-left: 25px;
-    padding: 2px 5px;
+    padding: 5px;
 `;
 const Input = styled.input`
     border: none;
@@ -73,7 +73,7 @@ const Navbar = () => {
           <Language>EN</Language>
           <SearchContainer>
             <Input></Input>
-            <SearchIcon></SearchIcon>
+            <SearchIcon style={{color: "gray", fontSize:16}}></SearchIcon>
           </SearchContainer>
         </Left>
         <Center><Logo>Brewtiful</Logo></Center>
