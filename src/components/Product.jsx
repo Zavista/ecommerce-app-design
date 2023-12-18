a7086e830b35a90fe30f4f5bd9d804a67aea9ffd
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 16:05:15 -0500

    Create Product and Products.jsx

---
+++ src/components/Product.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = sytled.div`
+    
+`
+const Product = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Product
---
+++ src/components/Products.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = sytled.div`
+    
+`
+const Products = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Products
