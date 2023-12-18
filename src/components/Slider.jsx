3f72f9a9ee821d9f6510e57417b9e62e297a9219
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-17 22:45:37 -0500

    Add initial slide in Slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -26,12 +26,65 @@ const Arrow = styled.div`
     opacity: 0.5;
 `
 
+const Wrapper = styled.div`
+    height: 100%;
+`
+
+const Slide = styled.div`
+    display: flex;
+    align-items:center;
+    width: 100vw;
+    height: 100vh;
+`
+const ImageContainer = styled.div`
+    flex: 1;
+    height: 100%
+`
+
+const Image = styled.img`
+    height: 80%
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+    
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Description = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 8px 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+    
+`
+
 const Slider = () => {
   return (
     <Container>
         <Arrow direction="left">
             <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
         </Arrow>
+        <Wrapper>
+            <Slide>            
+                <ImageContainer>
+                    <Image src="https://iili.io/H8Y78Qt.webp"></Image>
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>Summer Sale</Title>
+                    <Description>Don&apos;t Compromise on Coffee! Get A Flat 30% On Everything</Description>
+                    <Button>Shop Now</Button>
+                </InfoContainer>
+            </Slide>
+
+        </Wrapper>
         <Arrow direction="right">
             <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
         </Arrow>
