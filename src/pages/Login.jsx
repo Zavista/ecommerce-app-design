560e31ddb0932d112ce1a3bf6278cce630c807ac
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-19 14:29:34 -0500

    Create Container

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,15 @@
 import styled from "styled-components"
 
+const Container = styled.div`
+    
+`
+
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+        
+    </Container>
   )
 }
 
