3c431088c5f1f41e1d1e25f4c6ace976a94f0087
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 21:57:10 -0500

    Add Navbar

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-
+import Navbar from "../components/Navbar"
 const Container = styled.div`
     
 `
@@ -7,7 +7,7 @@ const Container = styled.div`
 const SingleProduct = () => {
   return (
     <Container>
-        Hi
+       <Navbar></Navbar>
     </Container>
   )
 }
