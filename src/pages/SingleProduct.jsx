b4a1f10d8362ef3a05e530b1bf333c5e2736919c
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 22:31:18 -0500

    Add fetch to get Product data

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,14 +1,65 @@
 import styled from "styled-components"
 import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+import { useState, useEffect } from "react"
 const Container = styled.div`
     
 `
+const Wrapper =styled.div`
+    
+`
+const ImageContainer = styled.div`
+    
+`
+const Image = styled.img`
+    
+`
+const InfoContainer = styled.div`
+    
+`
+const Title = styled.h1`
 
+`
+const Description = styled.p`
+    
+`
+const Price = styled.div`
+    
+`
 const SingleProduct = () => {
-  return (
-    <Container>
-       <Navbar></Navbar>
-    </Container>
+    const [item, setItem] = useState([]);
+    const id = 1; // Replace this with the dynamic ID you'll receive as a prop
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://fake-coffee-api.vercel.app/api/${id}`);
+                const data = await response.json();
+                setItem(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    return (
+        <Container>
+        <Navbar></Navbar>
+        <Wrapper>
+            <ImageContainer>
+                <Image src={item[0] && item[0].image_url}></Image>
+            </ImageContainer>
+            <InfoContainer>
+                <Title>{item[0] && item[0].name}</Title>
+                <Description>{item[0] && item[0].description}</Description>
+                <Price>{item[0] && item[0].price}</Price>
+            </InfoContainer>
+        </Wrapper>
+        <Newsletter></Newsletter>
+        <Footer></Footer>
+        </Container>
   )
 }
 
