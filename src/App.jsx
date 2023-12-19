556c3fe4771e6a1964a266e13f339555bdae1106
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-19 15:01:18 -0500

    Add Login for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import SingleProduct from './pages/SingleProduct'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <>
-      <Register></Register>
+      <Login></Login>
     </>
   )
 }
