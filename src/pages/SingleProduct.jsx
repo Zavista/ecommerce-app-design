c9eefb3e196ec051d4c863072d1566a86167e707
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-18 21:53:40 -0500

    Create Single Product page

---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    
+`
+
+const SingleProduct = () => {
+  return (
+    <Container>
+        Hi
+    </Container>
+  )
+}
+
+export default SingleProduct
