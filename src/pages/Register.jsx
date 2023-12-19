0f60d58d80fc30c468d736867b8341283c222395
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-19 14:45:56 -0500

    Add styled components to Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,56 @@
 import styled from "styled-components"
 
 const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.3),
+        rgba(255, 255, 255, 0.3)),
+    url(https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-cup-of-latte-on-wooden-table-image_2943328.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
     
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 400;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;;
 `
 
 const Register = () => {
   return (
     <Container>
-        <Wrapper></Wrapper>
+        <Wrapper>
             <Title>Create an Account</Title>
             <Form>
                 <Input placeholder="first name"></Input>
@@ -16,8 +59,11 @@ const Register = () => {
                 <Input placeholder="email"></Input>
                 <Input placeholder="password"></Input>
                 <Input placeholder="confirm password"></Input>
-                <Agreement>By creating an account, I consent to the processing of my personal data in accordance with <b>Privacy Policy</b></Agreement>
+                <Agreement>By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b>.</Agreement>
+                <Button>Create</Button>
             </Form>
+        </Wrapper>
+
     </Container>
   )
 }
