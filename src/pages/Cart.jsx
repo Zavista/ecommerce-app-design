a2a05f43c22293dc0803be90abe6190af77ed271
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-19 22:26:28 -0500

    Swap Add and Remove Icons

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,14 @@ import styled from "styled-components";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Add, Remove } from "@mui/icons-material";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({
+    padding: "10px",
+  })}
 `;
 const Title = styled.h1`
   font-weight: 400;
@@ -26,7 +30,11 @@ const TopButton = styled.button`
   background-color: ${(props) => (props.type === "filled" ? "teal" : "white")};
   color: ${(props) => (props.type === "filled" ? "white" : "teal")};
 `;
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({
+    display: "none",
+  })}
+`;
 const TopText = styled.span`
   text-decoration: underline;
   margin: 0px 10px;
@@ -34,6 +42,9 @@ const TopText = styled.span`
 
 const Bottom = styled.div`
   display: flex;
+  ${mobile({
+    flexDirection: "column",
+  })}
 `;
 
 const Info = styled.div`
@@ -44,10 +55,16 @@ const Info = styled.div`
 const ProductInfo = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({
+    flexDirection: "column",
+  })}
 `;
 const ProductDetail = styled.div`
   flex: 2;
   display: flex;
+  ${mobile({
+    flexDirection: "column",
+  })}
 `;
 const Image = styled.img`
   width: 100%;
@@ -57,11 +74,7 @@ const Image = styled.img`
 const ImageContainer = styled.div`
   width: 400px;
   position: relative;
-  &:hover {
-    transform: scale(1.4);
-  }
 `;
-
 
 const Detail = styled.div`
   padding: 20px;
@@ -69,21 +82,27 @@ const Detail = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  ${mobile({
+    padding: "20px",
+  })}
 `;
 
 const Product = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 const ProductName = styled.span`
-    font-size: 24px;
-    margin-bottom: 10px;
+  font-size: 24px;
+  margin-bottom: 10px;
 `;
 const ProductID = styled.span`
-    font-size: 16px;
+  font-size: 16px;
 `;
 const ProductGrind = styled.span`
-    font-size: 20px;
+  font-size: 20px;
+  ${mobile({
+    marginTop: "15px",
+  })}
 `;
 
 const PriceDetail = styled.div`
@@ -95,24 +114,29 @@ const PriceDetail = styled.div`
 `;
 
 const ProductAmountContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-    
-`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 const ProductAmount = styled.div`
-    font-size: 28px;
-    margin: 5px;
-`
+  font-size: 28px;
+  margin: 5px;
+  ${mobile({
+    margin: "5px 15px",
+  })}
+`;
 const ProductPrice = styled.div`
-    font-size: 36px;
-    font-weight: 200;
-`
+  font-size: 36px;
+  font-weight: 200;
+  ${mobile({
+    marginBottom: "25px",
+  })}
+`;
 const Hr = styled.hr`
-    background-color: #eee;
-    border: none;
-    height: 2px;
-`
+  background-color: #eee;
+  border: none;
+  height: 2px;
+`;
 
 const Summary = styled.div`
   flex: 1;
@@ -122,33 +146,28 @@ const Summary = styled.div`
 `;
 
 const SummaryTitle = styled.h1`
-    font-weight: 400;
-`
+  font-weight: 400;
+`;
 const SummaryItem = styled.div`
-    margin: 30px 0px;
-    display: flex;
-    justify-content: space-between;
-    
-    font-weight: ${props=>props.type === "total" && "500"};
-    font-size: ${props=>props.type === "total" && "24px"};
-`
-const SummaryItemText = styled.span`
-    
-`
-const SummaryItemPrice = styled.span`
-    
-`
-const Button = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: teal;
-    color: white;
-    border: 1px solid teal;
-    box-sizing: border-box;
-    cursor: pointer;
-    font-weight: 600;
-`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
 
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: teal;
+  color: white;
+  border: 1px solid teal;
+  box-sizing: border-box;
+  cursor: pointer;
+  font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -172,14 +191,14 @@ const Cart = () => {
                   <Image src="https://iili.io/H8Y78Qt.webp"></Image>
                 </ImageContainer>
                 <Detail>
-                    <Product>
-                        <ProductName>
-                            <b>Product: </b>Signature Blend
-                        </ProductName>
-                        <ProductID>
-                            <b>ID: </b> 123456789
-                        </ProductID> 
-                    </Product>
+                  <Product>
+                    <ProductName>
+                      <b>Product: </b>Signature Blend
+                    </ProductName>
+                    <ProductID>
+                      <b>ID: </b> 123456789
+                    </ProductID>
+                  </Product>
 
                   <ProductGrind>
                     <b>Grind: </b> Whole Bean
@@ -188,9 +207,9 @@ const Cart = () => {
               </ProductDetail>
               <PriceDetail>
                 <ProductAmountContainer>
-                    <Add></Add>
-                    <ProductAmount>2</ProductAmount>
-                    <Remove></Remove>
+                  <Remove></Remove>
+                  <ProductAmount>2</ProductAmount>
+                  <Add></Add>
                 </ProductAmountContainer>
                 <ProductPrice>$ 12.99</ProductPrice>
               </PriceDetail>
@@ -202,14 +221,14 @@ const Cart = () => {
                   <Image src="https://iili.io/H8Y7kTN.webp"></Image>
                 </ImageContainer>
                 <Detail>
-                    <Product>
-                        <ProductName>
-                            <b>Product: </b>Rainforest Rhapsody
-                        </ProductName>
-                        <ProductID>
-                            <b>ID: </b> 234567891
-                        </ProductID> 
-                    </Product>
+                  <Product>
+                    <ProductName>
+                      <b>Product: </b>Rainforest Rhapsody
+                    </ProductName>
+                    <ProductID>
+                      <b>ID: </b> 234567891
+                    </ProductID>
+                  </Product>
 
                   <ProductGrind>
                     <b>Grind: </b>Fine
@@ -218,34 +237,32 @@ const Cart = () => {
               </ProductDetail>
               <PriceDetail>
                 <ProductAmountContainer>
-                    <Add></Add>
-                    <ProductAmount>3</ProductAmount>
-                    <Remove></Remove>
+                  <Remove></Remove>
+                  <ProductAmount>3</ProductAmount>
+                  <Add></Add>
                 </ProductAmountContainer>
                 <ProductPrice>$ 14.99</ProductPrice>
               </PriceDetail>
             </ProductInfo>
           </Info>
 
-
-
           <Summary>
             <SummaryTitle>Order Summary</SummaryTitle>
             <SummaryItem>
-                <SummaryItemText>Signature Blend (2x)</SummaryItemText>
-                <SummaryItemPrice>$ 25.98</SummaryItemPrice>
+              <SummaryItemText>Signature Blend (2x)</SummaryItemText>
+              <SummaryItemPrice>$ 25.98</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
-                <SummaryItemText>Rainforest Rhapsody (3x)</SummaryItemText>
-                <SummaryItemPrice>$ 45.97</SummaryItemPrice>
+              <SummaryItemText>Rainforest Rhapsody (3x)</SummaryItemText>
+              <SummaryItemPrice>$ 45.97</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
-                <SummaryItemText>Estimated Shipping</SummaryItemText>
-                <SummaryItemPrice>$ 5.00</SummaryItemPrice>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.00</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem type="total">
-                <SummaryItemText>Total</SummaryItemText>
-                <SummaryItemPrice>$ 76.95</SummaryItemPrice>
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 76.95</SummaryItemPrice>
             </SummaryItem>
             <Button>Checkout Now</Button>
           </Summary>
