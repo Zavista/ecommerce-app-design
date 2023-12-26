d0decf00f04f7b5ac3db031ff55b50380a822068
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-26 14:49:02 -0500

    Removed Title and Button

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,25 +1,24 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { mobile } from "../responsive";
 
 const Container = styled.div`
   flex: 1;
   margin: 0px;
-  margin-bottom: 1%;
   height: 50vh;
-  position: relative;  
+  position: relative;
   background-color: transparent;
-  &:hover{
-    transform: scale(1.1);
+  &:hover {
+    transform: scale(1.2);
   }
   ${mobile({
-    height: "30vh"
+    height: "30vh",
   })}
-`
+`;
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-`
+`;
 
 const Info = styled.div`
   width: 100%;
@@ -30,35 +29,32 @@ const Info = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: space-between
-`
+  justify-content: space-between;
+`;
 
 const Title = styled.h1`
   color: black;
   padding-top: 7%;
   ${mobile({
-    fontSize: "24px"
+    fontSize: "24px",
   })}
-`
+`;
 const Button = styled.button`
   border: 1px solid black;
   padding: 10px 20px;
-  background-color: white;
+  background-color: transparent;
   color: gray;
   cursor: pointer;
   margin-bottom: 5%;
-`
+`;
 
-const CategoryItem = ({item}) => {
+
+const CategoryItem = ({ item }) => {
   return (
     <Container>
       <Image src={item.img}></Image>
-      <Info>
-          <Title>{item.title}</Title>
-          <Button>Explore</Button>
-      </Info>
     </Container>
-  )
-}
+  );
+};
 
-export default CategoryItem
+export default CategoryItem;
