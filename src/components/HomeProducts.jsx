8bc2a453676476f44f57f01cc95c5f2d8a374ad6
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-12-26 15:04:48 -0500

    Create HomeProducts page

---
+++ src/components/HomeProducts.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+import Products from "./Products"
+import { mobile } from "../responsive"
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const Title = styled.h1`
+  font-weight: 500;
+  font-size: 40px;
+  margin-top: 40px;
+  ${mobile({
+    fontSize: "28px",
+  })}
+`
+
+const HomeProducts = () => {
+  return (
+    <Container>
+        <Title>Popular Products</Title>
+        <Products style={{flex:}}></Products>
+    </Container>
+  )
+}
+
+export default HomeProducts
